dedf921bbd60f639dc7d9d7d12d47c43f027c39b
Eleftheria Chatzidaki <echatzidaki@isc.tuc.gr>
2020-06-29 02:24:07 +0300

    Update robot_class.py

--- robot_class.py
+++ robot_class.py
@@ -65,7 +65,6 @@ class robot:
     #        landmarks to be visible at all times
     #
     
-    ## TODO: paste your complete the sense function, here
     ## make sure the indentation of the code is correct
     def sense(self):
         ''' This function does not take in any parameters, instead it references internal variables
@@ -79,20 +78,27 @@ class robot:
            
         measurements = []
         
-        ## TODO: iterate through all of the landmarks in a world
-        
-        ## TODO: For each landmark
+        ## iterate through all of the landmarks in a world
+        for i, landmark in enumerate(self.landmarks):
+        ##  For each landmark
         ## 1. compute dx and dy, the distances between the robot and the landmark
+            dx = landmark[0] - self.x
+            dy = landmark[1] - self.y
         ## 2. account for measurement noise by *adding* a noise component to dx and dy
         ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
         ##    - Feel free to use the function self.rand() to help calculate this noise component
+        ##    - It may help to reference the `move` function for noise calculation
+            dx = dx + self.rand()*self.measurement_noise
+            dy = dy + self.rand()*self.measurement_noise
         ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
         ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
         ##    as list.append([index, dx, dy]), this format is important for data creation done later
-        
-        ## TODO: return the final, complete list of measurements
+            if abs(dx) <= self.measurement_range and abs(dy) <= self.measurement_range:
+                    measurements.append([i, dx, dy])
+                
+        ## Return the final, complete list of measurements
         return measurements
-
+    
 
     # --------
     # make_landmarks:
